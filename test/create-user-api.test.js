ab637e3e30d6670985972dca83f17ac8e547b490
Brenda Granados <bgran05@gmail.com>
2023-06-09 18:09:51 -0500

    Use uuid library to make unique email

--- test/create-user-api.test.js
+++ test/create-user-api.test.js
@@ -1,6 +1,10 @@
+const { v4: uuidv4 } = require('uuid');
+
 const { expect } = require("chai");
 
 const token = "bee47dc252fdb871aa7b3899e4cd9dff1ab2607891298226c0a69eedfeb5a651";
+
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 const headers={
     'Authorization': `Bearer ${token}`
@@ -45,10 +49,11 @@ describe('User API ', ()=>{
 
 
     // Create new user
+    const userEmailGuid = uuidv4();
     const testUser = {
         'name':'TestUser', 
         'gender':'male', 
-        'email':'bg@15ce.com', 
+        'email':`bg+${userEmailGuid}@cd.com`, 
         'status':'active'
     }
 
@@ -60,10 +65,9 @@ describe('User API ', ()=>{
     }
 
     // TBD: this test would work exactly 1x, need to randomize email
-    it('Create User, creates a user with POST, returns 200', async () => {
+    it('Create User, creates a user with POST, returns 201', async () => {
         await fetch(usersApi, {headers: headersToAddContent, method: 'POST', body: JSON.stringify(testUser)}).then(async response => {
             var responseJson = await response.json()               
-            console.log(responseJson)
             expect(response.status).to.be.equal(201);              
         })
     })
