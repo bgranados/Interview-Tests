20cce7f5f476f1959c7a3c469128c3be3bf48994
Brenda Granados <bgran05@gmail.com>
2023-06-10 17:21:22 -0500

    significant update to get an id by email

--- test/create-user-api.test.js
+++ test/create-user-api.test.js
@@ -6,9 +6,50 @@ const token = "bee47dc252fdb871aa7b3899e4cd9dff1ab2607891298226c0a69eedfeb5a651"
 
 uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
-const headers={
-    'Authorization': `Bearer ${token}`
+/** Include the Bearer Token for these headers */
+var authenticatedHeaders =
+{
+   "Authorization": `Bearer ${token}`,
+   "Accept":"application/json", 
+   "Content-Type":"application/json"
 }
+
+//TBD: refactor
+function createNewUser(){
+     // Create new user
+     const userEmailGuid = uuidv4();
+     const testUser = {
+         'name':'TestUser', 
+         'gender':'male', 
+         'email':`bg+${userEmailGuid}@cd.com`, 
+         'status':'active'
+     }
+    return testUser;
+}
+
+
+// use javascript Object filter. example: 
+// https://stackoverflow.com/questions/61229242/how-to-filter-json-object-javascript
+async function getUserIdByEmail(email){
+    var userId= 0;
+    await fetch('https://gorest.co.in/public/v2/users', {headers: authenticatedHeaders, method: 'GET'}).then(async (response) => {
+       
+        expect(response.status).to.be.equal(200);
+        var responseJson = await response.json()          
+        const items = responseJson;
+        expect(responseJson[0].id, 'Could not find any user IDs').to.exist;  //return at least one user
+
+        const userResult = items.filter(c => c.email === email)  //with plain  = you are setting that email. :(
+        console.log("filtered results", userResult)
+
+        console.log("looking for user with email ", email)
+        console.log('user result is ', userResult[0].id)
+        userId = userResult[0].id
+        resolve();
+    })
+    return userId;
+}
+
 // fetch is natively supported by Node to make HttpRequests
 // https://developer.mozilla.org/en-US/docs/Web/API/fetch
 // https://javascript.info/fetch for examples
@@ -17,8 +58,10 @@ describe('User API ', ()=>{
 
     
     // NOTE: GET does not require any headers for this API.
+    // If you want to return users created by YOUR token, you will need to provide it.
+    // Otherwise GET returns a generic set.
 
-    it('GET can get user list without header', async ()=>{
+    it.skip('GET can get user list without header', async ()=>{
         await fetch('https://gorest.co.in/public/v2/users').then(async response => {
             expect(response.status).to.be.equal(200);
             var responseJson = await response.json()          
@@ -26,20 +69,20 @@ describe('User API ', ()=>{
         })
     })
     
-    it('Create User, missing header with POST, returns 401', async () => {
+    it.skip('Create User, missing header with POST, returns 401', async () => {
         await fetch(usersApi, {method: 'POST'}).then(response => {
             expect(response.status).to.be.equal(401)
         })
     })
 
     // PAGE NOT FOUND
-    it('Create User, page not found for method PUT, returns 404', async () => {
+    it.skip('Create User, page not found for method PUT, returns 404', async () => {
         await fetch(usersApi, {method: 'PUT'}).then(response => {
             expect(response.status).to.be.equal(404)
         })
     })
 
-    it('Create User, page not found for method DELETE, returns 404', async () => {
+    it.skip('Create User, page not found for method DELETE, returns 404', async () => {
         await fetch(usersApi, {method: 'DELETE'}).then(response => {
             expect(response.status).to.be.equal(404)
         })
@@ -55,22 +98,15 @@ describe('User API ', ()=>{
         'status':'active'
     }
 
-    var headersToAddContent =
-     {
-        "Authorization": `Bearer ${token}`,
-        "Accept":"application/json", 
-        "Content-Type":"application/json"
-    }
-
     // TBD: this test would work exactly 1x, need to randomize email
-    it('Create User, creates a user with POST, returns 201', async () => {
-        await fetch(usersApi, {headers: headersToAddContent, method: 'POST', body: JSON.stringify(testUser)}).then(async response => {
+    it.skip('Create User, creates a user with POST, returns 201', async () => {
+        await fetch(usersApi, {headers: authenticatedHeaders, method: 'POST', body: JSON.stringify(testUser)}).then(async response => {
             var responseJson = await response.json()               
             expect(response.status).to.be.equal(201);              
         })
     })
 
-    it('Create user, POST fails to create user with no email, returns 422', async () => {
+    it.skip('Create user, POST fails to create user with no email, returns 422', async () => {
         const testUserMissingEmail = {
             'name':'TestUser', 
             'gender':'male', 
@@ -78,7 +114,7 @@ describe('User API ', ()=>{
             'status':'active'
         }
 
-        await fetch(usersApi, {headers: headersToAddContent, method: 'POST', body: JSON.stringify(testUserMissingEmail)})
+        await fetch(usersApi, {headers: authenticatedHeaders, method: 'POST', body: JSON.stringify(testUserMissingEmail)})
             .then(async response => {
                 var responseJson = await response.json();
                 expect(responseJson[0]).to.have.property('message')
@@ -87,5 +123,47 @@ describe('User API ', ()=>{
                 expect(responseJson[0].field).contains('email')
                 expect(response.status).to.be.equal(422)
         })
+    })
+
+    it('can DELETE user, returns 204 - no content', async () => {
+        // arrange
+
+        var testUserDelete = createNewUser()
+
+        console.log('get user id by email')
+        var userIdToDelete = "0";
+
+
+        await fetch(usersApi, {headers: authenticatedHeaders, method: 'POST', body: JSON.stringify(testUserDelete)}).then(async (response)=>{
+            var responseJson = await response.json()               
+            expect(response.status).to.be.equal(201);
+            console.log('created user with email ', testUserDelete.email)
+            //expect(createdUserObject.id).to.exist;
+            
+            //var createdUserId = createdUserObject.id;
+            //console.log('created user id ', createdUserId)
+    
+            userIdToDelete = await getUserIdByEmail(testUserDelete.email);
+            console.log('user id to delete, filtered', userIdToDelete)
+        })
+        
+       
+        // act
+      /*  await fetch(`${usersApi}/${userIdToDelete}`, {headers: authenticatedHeaders, method: 'DELETE'}).then(async (response) =>{
+            expect(response.status).to.be(204)
+        })*/
+
+
+    })
+
+    it('tries to DELETE user by non existent id, returns 404', async () => {
+        var userIdToDelete  = '0000'
+        await fetch(`${usersApi}/${userIdToDelete}`, {headers: authenticatedHeaders, method: 'DELETE'}).then(async (response) =>{
+            expect(response.status).to.equal(404)
+        })
+    })
+
+    it('can GET user, by ID, returns 200 and user object', async () => {
+
     })
 })
